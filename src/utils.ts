a1a7a0fb3f0bf82abcb7a747634e040bbcedde1b
Tony Aldridge <tony@dabapps.com>
2017-11-16 13:24:04 +0000

    Stricter types on TypeToRecordMapping

--- src/utils.ts
+++ src/utils.ts
@@ -1,2 +1,2 @@
 export type Dict<T> = Readonly<{[k: string]: T}>;
-export type TTypeToRecordMapping<T> = {[K in keyof T]: (({}) => T[K]) };
+export type TTypeToRecordMapping<T, U extends {[key: string]: any} = {}> = {[K in keyof T]: ((input: U) => T[K]) };
