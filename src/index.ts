1032e76ee3f0110ec563c354181e6daf6d02ee83
Danny <dannyrobinson94@hotmail.com>
2018-08-21 16:35:57 +0100

    Exposing generic requests

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,11 @@ import { itemsFunctor } from './items';
 import { CollectionOptions } from './types';
 import { IdKeyedMap, TypeToRecordMapping } from './utils';
 
+export { dispatchGenericRequest } from './requests/actions'
+export { makeAsyncActionSet } from './requests/utils'
+export { AsyncActionSet, UrlMethod, RequestMetaData } from './requests/types'
+
+
 export function Collections<T extends IdKeyedMap<T>, U extends IdKeyedMap<U>>(
   collectionToRecordMapping: TypeToRecordMapping<T>,
   itemToRecordMapping: TypeToRecordMapping<U>,
